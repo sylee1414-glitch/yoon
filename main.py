7781a5756de4df8e0b0f7bd08a0fa8fe0eef29ac
sylee1414-glitch <sylee1414@gmail.com>
2026-02-26 13:28:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="홈페이지", page_icon="🏠")
 st.title('나의 첫 웹 서비스 만들기!')
 name = st.text_input('이름을 입력해주세요: ')
 menu = st.selectbox('좋아하는 음식을 선택해주세요: ', ['슈팅스타', '아몬드봉봉'])
